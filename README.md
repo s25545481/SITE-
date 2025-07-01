005ca12f4664d2e0094204f362eb991849fa4168
s25545481 <143324950+s25545481@users.noreply.github.com>
2025-07-01 05:42:16 +0330

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Old Team Simple
+نسخه ساده‌شده از سایت Old Team بدون وابستگی UI اضافی.
---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OldTeamApp from "./OldTeamApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <OldTeamApp />
+  </React.StrictMode>
+);
