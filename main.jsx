20271576338aea6995b987b6828148761cba5fca
s25545481 <143324950+s25545481@users.noreply.github.com>
2025-07-01 05:41:33 +0330

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import OldTeamApp from './OldTeamApp.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <OldTeamApp />
-  </React.StrictMode>
-);
